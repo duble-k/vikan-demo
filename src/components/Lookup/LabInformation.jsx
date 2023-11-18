8c4de9e43731259bcdbbd9113a0563effee184b9
dublek <karbasi02@gmail.com>
2023-11-18 15:15:30 -0500

    fixing url wrapping

--- src/components/Lookup/LabInformation.jsx
+++ src/components/Lookup/LabInformation.jsx
@@ -19,70 +19,70 @@ const LabInformation = ({ result }) => {
       <Grid container spacing={2}>
         {/* Left Column */}
         <Grid item xs={5}>
-        {result.testingSite && (
-            <>
-                <Typography style={{ color: 'black' }}>Testing Site:</Typography>
-                <Typography style={{ color: 'grey' }}>{result.testingSite}</Typography>
-            </>
-          )}
-        {result.orderingInstructions && (
-            <>
-                <Typography style={{ color: 'black' }}>Ordering Instructions:</Typography>
-                <Typography style={{ color: 'grey' }}>{result.orderingInstructions}</Typography>
-            </>
-          )}
-        {result.container && (
-            <>
-                <Typography style={{ color: 'black' }}>Container:</Typography>
-                <Typography style={{ color: 'grey' }}>{result.container}</Typography>
-            </>
-          )}
-        {result.frequency && (
-            <>
-                <Typography style={{ color: 'black' }}>Frequency:</Typography>
-                <Typography style={{ color: 'grey' }}>{result.frequency}</Typography>
-            </>
-          )}
-        {result.TAT && (
-            <>
-                <Typography style={{ color: 'black' }}>TAT:</Typography>
-                <Typography style={{ color: 'grey' }}>{result.TAT}</Typography>
-            </>
-          )}
-        {result.stabilityForAddOn && (
-            <>
-                <Typography style={{ color: 'black' }}>Stability for Add-On:</Typography>
-                <Typography style={{ color: 'grey' }}>{result.stabilityForAddOn}</Typography>
-            </>
-          )}
+          {result.testingSite && (
+              <>
+                  <Typography style={{ color: 'black' }}>Testing Site:</Typography>
+                  <Typography style={{ color: 'grey' }}>{result.testingSite}</Typography>
+              </>
+            )}
+          {result.orderingInstructions && (
+              <>
+                  <Typography style={{ color: 'black' }}>Ordering Instructions:</Typography>
+                  <Typography style={{ color: 'grey' }}>{result.orderingInstructions}</Typography>
+              </>
+            )}
+          {result.container && (
+              <>
+                  <Typography style={{ color: 'black' }}>Container:</Typography>
+                  <Typography style={{ color: 'grey' }}>{result.container}</Typography>
+              </>
+            )}
+          {result.frequency && (
+              <>
+                  <Typography style={{ color: 'black' }}>Frequency:</Typography>
+                  <Typography style={{ color: 'grey' }}>{result.frequency}</Typography>
+              </>
+            )}
+          {result.TAT && (
+              <>
+                  <Typography style={{ color: 'black' }}>TAT:</Typography>
+                  <Typography style={{ color: 'grey' }}>{result.TAT}</Typography>
+              </>
+            )}
+          {result.stabilityForAddOn && (
+              <>
+                  <Typography style={{ color: 'black' }}>Stability for Add-On:</Typography>
+                  <Typography style={{ color: 'grey' }}>{result.stabilityForAddOn}</Typography>
+              </>
+            )}
         </Grid>
-
         {/* Vertical Divider */}
         <Grid item xs={1}>
           <Divider orientation="vertical" style={{ height: '100%' }} />
         </Grid>
-
         {/* Right Column */}
         <Grid item xs={6}>
-        {result.collectionInstructions && (
-            <>
-                <Typography style={{ color: 'black' }}>Collection Instructions:</Typography>
-                <Typography style={{ color: 'grey' }}>{result.collectionInstructions}</Typography>
-            </>
-          )}
-        {result.externalLink && (
-            <>
-                <Typography style={{ color: 'black' }}>For all other information:</Typography>
-                <Link
-                    href={result.externalLink}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    style={{ color: 'blue', cursor: 'pointer' }}
-                    >
-                    {result.externalLink}
-                </Link>
-            </>
-          )}
+          {result.collectionInstructions && (
+              <>
+                  <Typography style={{ color: 'black' }}>Collection Instructions:</Typography>
+                  <Typography style={{ color: 'grey' }}>{result.collectionInstructions}</Typography>
+              </>
+            )}
+          {result.externalLink && (
+              <>
+                  <Typography style={{ color: 'black' }}>For all other information:</Typography>
+                  <Link
+                      href={result.externalLink}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      style={{ color: 'blue',
+                               cursor: 'pointer',
+                               wordWrap: 'break-word' }}
+                      >
+                      {result.externalLink}
+                  </Link>
+              </>
+            )}
         </Grid>
       </Grid>
     </div>
